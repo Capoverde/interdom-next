2b17d5c96a51677f8b20dbe0985c48836259fa83
Capoverde <piotr_as@yahoo.com>
2024-01-05 19:13:36 +0100

    problem with Navbar toggling

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,52 +1,43 @@
-// Header.jsx
-"use client"
+import { useState } from 'react';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { Rotate as Hamburger } from 'hamburger-react';
-import { Link } from 'next/link'
+import Link from 'next/link';
 import './Header.scss';
 
-export const Header = () => {
+export const Header = ({ toggleMenu }) => {
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <header className='page-header w-full  flex justify-between xl:h-[10vh] font-bold fixed border-b grid-border-clr'>
       <div className="header-box flex items-center justify-center h-full w-[5%] border-r grid-border-clr">
-        <Hamburger className="z-50" />
+        <Hamburger
+          className="z-50"
+          onClick={() => {
+            toggleMenu();
+            setOpen((prevOpen) => !prevOpen);
+          }}
+          toggled={isOpen}
+          toggle={setOpen}
+        />
       </div>
       <div className="header-box flex items-center justify-center h-full w-[10%] border-r grid-border-clr">
-        <Link
-         href='#'
-           O nas
-        ></Link>
+        <Link href='#'>O nas</Link>
       </div>
       <div className="header-box flex items-center justify-center h-full w-[15%] border-r grid-border-clr">
-        <Link
-         href='#'
-          Oferta
-        ></Link>
+        <Link href='#'>Oferta</Link>
       </div>
       <div className="header-box flex items-center justify-center h-full grow border-r grid-border-clr">
-        <Link
-         href='/'
-          Interdom
-        ></Link>
+        <Link href='/'>Interdom</Link>
       </div>
       <div className="header-box flex items-center justify-center h-full w-[15%] border-r grid-border-clr">
-        <Link
-         href='#'
-          e-kartoteka
-        ></Link>
+        <Link href='#'>e-kartoteka</Link>
       </div>
       <div className="header-box flex items-center justify-center h-full w-[10%] border-r grid-border-clr">
-        <Link
-         href='#'
-           Kontakt
-        ></Link>
+        <Link href='#'>Kontakt</Link>
       </div>
       <div className="header-box flex items-center justify-center h-full w-[5%] ">
         <FaPhoneAlt className="text-xl" />
       </div>
     </header>
   );
-}
-
-// export default Header;
+};
--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
-import Navbar from "./components/Navbar/Navbar"
-import  {Header}  from "./components/Header/Header"
+"use client"
+import Navbar from "./components/Navbar/Navbar";
+import { Header } from "./components/Header/Header";
+import { useState } from "react";
 
 export default function Home() {
-  return(
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+    console.log('click w hamburger');
+  };
+
+  return (
     <>
-     <Navbar />
-     <Header />
+      <Header toggleMenu={toggleMenu} />
+      <Navbar isOpen={menuOpen} toggleMenu={toggleMenu} />
     </>
-  )  
-  
+  );
 }
