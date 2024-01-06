20de12a54725c8f4c900c86c5d30c77b8708385a
Capoverde <piotr_as@yahoo.com>
2024-01-06 13:05:54 +0100

    starting AboutPage & ScoreBanner

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,56 +1,3 @@
-// import { FaPhoneAlt } from 'react-icons/fa';
-// import { Rotate as Hamburger } from 'hamburger-react';
-// import Link from 'next/link';
-// import { useState } from 'react';
-// import './Header.scss';
-
-// export const Header = ({ toggleMenu }) => {
-//   const [isOpen, setOpen] = useState(false);
-
-//   const handleToggle = (toggled) => {
-//     toggleMenu();
-//     setOpen(toggled);
-//   };
-
-//   return (
-//     <header className='page-header w-full  flex justify-between xl:h-[10vh] font-bold fixed border-b grid-border-clr'>
-//       <div className="header-box hamburger-box flex items-center justify-center h-full w-[5%] border-r grid-border-clr">
-//         <Hamburger
-//           className="z-[100]"
-//           toggled={isOpen}
-//           toggle={setOpen}
-//           onToggle={handleToggle}
-//         />
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full w-[20%] md:w-[10%] border-r grid-border-clr">
-//         <Link href='#'>O nas</Link>
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full w-[15%] border-r grid-border-clr">
-//         <Link href='#'>Oferta</Link>
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full grow border-r grid-border-clr">
-//         <Link href='/'>Interdom</Link>
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full w-[15%] border-r grid-border-clr">
-//         <Link href='#'>e-kartoteka</Link>
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full w-[10%] border-r grid-border-clr">
-//         <Link 
-//         href='/ContactPage/Contact'
-
-//         >Kontakt</Link>
-//       </div>
-//       <div className="header-box flex items-center justify-center h-full w-[5%] ">
-//         <Link 
-//          href='tel:+486308437'
-//         >
-//          <FaPhoneAlt className="text-xl" />
-//         </Link>
-//       </div>
-//     </header>
-//   );
-// };
-
 import { FaPhoneAlt } from 'react-icons/fa';
 import { Rotate as Hamburger } from 'hamburger-react';
 import Link from 'next/link';
@@ -64,6 +11,10 @@ export const Header = ({ toggleMenu }) => {
     toggleMenu();
     setOpen(toggled);
   };
+
+  const phones = {
+    phone1: '+486308437'
+  }
 
   return (
     <header className={`page-header w-full flex justify-between xl:h-[10vh] font-bold fixed border-b grid-border-clr ${isOpen ? 'menu-open' : ''}`}>
@@ -92,9 +43,10 @@ export const Header = ({ toggleMenu }) => {
           href='/ContactPage/Contact'
         >Kontakt</Link>
       </div>
-      <div className={`header-box flex items-center justify-center h-full w-[5%] ${isOpen ? 'menu-open' : ''}`}>
+      <div className={`header-box  h-full w-[5%] ${isOpen ? 'menu-open' : ''}`}>
         <Link 
-          href='tel:+486308437'
+          href={`tel:${phones.phone1}`}
+          className='w-full h-full flex items-center justify-center'
         >
           <FaPhoneAlt className="text-xl" />
         </Link>
