8d1eb67f0a1c5d4b8e7d1118ede7beb75b78d18e
Capoverde <piotr_as@yahoo.com>
2024-01-11 00:29:08 +0100

    second faze of accordion animation

--- app/components/OfferAccordion/OfferAccordion.jsx
+++ app/components/OfferAccordion/OfferAccordion.jsx
@@ -1,23 +1,68 @@
-import React from 'react'
-import  './OfferAccordion.scss'
-import AccordionData from './AccordionData.json'
+import React, { useState } from 'react';
+import { motion as m, useAnimation } from 'framer-motion';
+import './OfferAccordion.scss';
+import AccordionData from './AccordionData.json';
 
 export const OfferAccordion = () => {
+  const [hoveredItem, setHoveredItem] = useState(null);
+  const controls = useAnimation();
+
+  const handleMouseEnter = (id) => {
+    setHoveredItem(id);
+    controls.start({
+      opacity: 1,
+      y: 0,
+      transition: { 
+        duration: 0,
+        ease: 'easeInOut' 
+      },
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+    controls.start({
+      opacity: 0,
+      y: 20,
+      transition: { duration: 0, delay: 0 },
+    });
+  };
+
   return (
-    <div className='w-full felx-col xl:flex md:flex'>
-      {AccordionData.map((data)=> (
-        <div key={data.id} className="accordionBox w-full relative border-r grid-border-clr cursor-pointer overflow-hidden ">
-          <div className="content w-full h-full">
-           <h3 className='offerTitle text-[4rem] text-gray-200 p-8'>
-            {data.title}
-          </h3>
-            <p className='offerText p-8 text-gray-200 z-[30] opacity-0 '>  
-             {data.describe}
-            </p>
-           </div>
+    <div className='w-full xl:flex md:flex overflow-hidden'>
+      {AccordionData.map((data) => (
+        <div
+          key={data.id}
+          className={`accordionBox w-full border-r grid-border-clr cursor-pointer overflow-hidden ${
+            hoveredItem === data.id ? 'hovered' : ''
+          }`}
+          onMouseEnter={() => handleMouseEnter(data.id)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className='content relative w-full h-full'>
+            <h3 className='offerTitle text-[4rem] text-gray-200 p-8'>
+              {data.title}
+            </h3>
+            <m.p
+              className='offerText p-8 text-gray-200 z-[30]'
+              initial={{ opacity: 0, y: 20 }}
+              animate={{
+                opacity: hoveredItem === data.id ? 1 : 0,
+                y: hoveredItem === data.id ? 0 : 20,
+              }}
+              transition={{
+                duration: hoveredItem === data.id ? 0.25 : 0,
+                ease: 'easeInOut',
+                delay: hoveredItem === data.id ? 0.5 : 0,
+              }}
+              controls={controls}
+            >
+              {data.describe}
+            </m.p>
+          </div>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
