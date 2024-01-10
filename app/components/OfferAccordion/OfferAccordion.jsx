4b5761a17350f1409303af744e0d0524ffc093bc
Capoverde <piotr_as@yahoo.com>
2024-01-10 21:43:56 +0100

    animation in accordion - beginning

--- app/components/OfferAccordion/OfferAccordion.jsx
+++ app/components/OfferAccordion/OfferAccordion.jsx
@@ -6,12 +6,12 @@ export const OfferAccordion = () => {
   return (
     <div className='w-full felx-col xl:flex md:flex'>
       {AccordionData.map((data)=> (
-        <div key={data.id} className="accordionBox w-full relative border-r grid-border-clr cursor-pointer ">
-          <div className="content">
-           <h3 className='title text-5xl text-slate-9650 p-8'>
+        <div key={data.id} className="accordionBox w-full relative border-r grid-border-clr cursor-pointer overflow-hidden ">
+          <div className="content w-full h-full">
+           <h3 className='offerTitle text-[4rem] text-gray-200 p-8'>
             {data.title}
           </h3>
-            <p className='text p-8'>  
+            <p className='offerText p-8 text-gray-200 z-[30] opacity-0 '>  
              {data.describe}
             </p>
            </div>
