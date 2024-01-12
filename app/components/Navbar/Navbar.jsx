08effe10c67fb6010588d806d09e8c839ba71ae0
Capoverde <piotr_as@yahoo.com>
2024-01-12 17:30:13 +0100

    left side box animation address working

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -18,6 +18,7 @@ const contactItems = [
   {id: 4, day: "Czwartek", hours: "09:00 - 16:00"},
   {id: 5, day: "Piątek", hours: "09:00 - 16:00"},
   {id: 6, address: "ul. Rewolucji 1905 r. Nr 7, lok. 3/3a"},
+  {id: 7, addressHeader: "adres biura:"}
 ]
 
 export const Navbar = ({ toggleMenu, isOpen }) => {
@@ -75,7 +76,7 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
   const NavLinks = ({ title, href }) => {
   
     return (
-      <m.div variants={centerLinkVars} className={`navLink text-5xl mt-8 uppercase relative text-gray-200 `}>
+      <m.div variants={centerLinkVars} className={`navLink text-5xl mt-8 uppercase  text-gray-200 `}>
         <Link href={href} onClick={() => {toggleMenu();}}>
           <span className="NavLinkSpan">{title}</span>
         </Link>
@@ -83,65 +84,85 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
     );
   };
 
+  const AddressTitle = () => {
+    return(
+     <m.h3 className="text-3xl uppercase" variants={centerLinkVars}>Adres Biura:</m.h3>
+    )
+  }
+
+  const InfoLinks = ({ address, addressHeader }) => {
+  
+    return (
+      <m.div variants={centerLinkVars} className="mt-8 uppercase text-gray-200">
+          <div className="pb-10 w-full">
+          {contactItems.map((address, id) => (
+            <m.div className="mt-3 overflow-hidden" key={address.id} variants={centerLinkVars}>
+              <Link
+                href="https://www.google.com/maps/place/Łódź, ul. Rewolucji 1905 r. nr 7"
+                target="_blank"
+                title="kliknij aby sprawdzić w google maps"
+              >
+                {address.address}
+              </Link>
+            </m.div>
+          ))}                
+        </div>
+      </m.div>
+    );
+  };
+
   return (
-      <AnimatePresence>
-        {isOpen && (
-          <m.nav
+    <AnimatePresence>
+      {isOpen && (
+        <m.nav
+          variants={containerVars}
+          initial="initial"
+          animate="open"
+          exit="initial"
+          className={`page-nav flex fixed left-0 w-full h-screen origin-top 
+                    border-t border-[#3e3e3e]
+                    bg-slate-950 text-gray-200 z-[50]`}
+        >
+          {/* ----------------- LEFT ------------------ */}
+          <m.div className="w-full h-full  flex-1 xl:border-r border-[#3e3e3e] xl:px-11"
             variants={containerVars}
             initial="initial"
             animate="open"
             exit="initial"
-            className={`page-nav flex fixed left-0 w-full h-screen origin-top 
-                        border-t border-[#3e3e3e]
-                      bg-slate-950 text-gray-200 z-[50]`}
           >
-            {/* ----------------- LEFT ------------------ */}
-            <div className="w-full h-full grid place-content-center flex-1 xl:border-r border-[#3e3e3e] xl:px-11">
-            <div className="address mb-10 w-full">
-                <h3 className="text-3xl  uppercase" >
-                  Adres biura:
-                </h3>
-                {contactItems.map((address, id) => (
-                      <address className="mt-3" key={address.id}>
-                        <Link
-                         href="https://www.google.com/maps/place/Łódź, ul. Rewolucji 1905 r. nr 7"
-                         target="_blank"
-                         title="kliknij aby sprawdzić w google maps"
-                        >
-                         {address.address}
-                        </Link>
-                      </address>
-                  ))}                
+            <div className="overflow-hidden" >
+              <AddressTitle />
+            </div>
+            <div className="overflow-hidden" >
+              <InfoLinks />
+            </div>
+            <h3 className="text-3xl xl:mb-6  uppercase">dni i godziny pracy biura:</h3>
+            {contactItems.map((item) =>(
+              <div className="flex justify-between" key={item.id}>
+                <div className="py-2">{item.day}</div>
+                <div className="py-2">{item.hours}</div>
               </div>
-              <h3 className="text-3xl xl:mb-6  uppercase">dni i godziny pracy biura:</h3>
-              {contactItems.map((item) =>(
-                <div className="flex justify-between" key={item.id}>
-                    <div className="py-2">{item.day}</div>
-                    <div className="py-2">{item.hours}</div>
+            ))}
+          </m.div>
+          {/* ----------------- CENTER ------------------ */}
+          <div className="h-full flex-1 flex-col border-r border-[#3e3e3e] px-8">
+            <m.div
+              variants={containerVars}
+              initial="initial"
+              animate="open"
+              exit="initial"
+              className="flex flex-col h-full  items-center gap-10 md:mb-12 xl:mb-12"
+            >
+              {navLinks.map((link, index) => (
+                <div className="overflow-hidden" key={index}>
+                  <NavLinks title={link.title} href={link.href} />
                 </div>
               ))}
-
-            </div>
-            {/* ----------------- CENTER ------------------ */}
-            <div className="h-full flex-1 flex-col border-r border-[#3e3e3e] px-8">
-              <m.div
-                variants={containerVars}
-                initial="initial"
-                animate="open"
-                exit="initial"
-                className="flex flex-col h-full  items-center gap-10 md:mb-12 xl:mb-12"
-              >
-                {navLinks.map((link, index) => (
-                  <div className="overflow-hidden" key={index}>
-                    <NavLinks title={link.title} href={link.href} />
-                  </div>
-                ))}
-              </m.div>
-            </div>
-            <div className="w-full h-full flex-1"></div>
-          </m.nav>
-        )}
-      </AnimatePresence>
+            </m.div>
+          </div>
+          <div className="w-full h-full flex-1"></div>
+        </m.nav>
+      )}
+    </AnimatePresence>
   );
 };
-
