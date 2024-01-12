3bdfd632c45f67924ff578d275363f28d415811a
Capoverde <piotr_as@yahoo.com>
2024-01-12 19:26:37 +0100

    left menu bo animation works correctly

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -18,7 +18,9 @@ const contactItems = [
   {id: 4, day: "Czwartek", hours: "09:00 - 16:00"},
   {id: 5, day: "Piątek", hours: "09:00 - 16:00"},
   {id: 6, address: "ul. Rewolucji 1905 r. Nr 7, lok. 3/3a"},
-  {id: 7, addressHeader: "adres biura:"}
+  {id: 7, phone1: "+42 630 84 55"},
+  {id: 8, phone2: "530 223 625"},
+  {id: 9, mail: "biuro@interdom-nieruchomosci.pl"},
 ]
 
 export const Navbar = ({ toggleMenu, isOpen }) => {
@@ -40,7 +42,7 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
       y:"-100%",
       transition: {
         duration: 0.5,
-        delay: 1,
+        delay: 0.8,
         staggerChildren: 0.09,
         staggerDirection: -1,
       },
@@ -58,7 +60,7 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
 
   const centerLinkVars = {
     initial: {
-      y: "10vh",
+      y: "8vh",
       transition: {
         duration: 0.5,
         ease: [0.37, 0, 0.63, 1],
@@ -74,7 +76,6 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
   };
 
   const NavLinks = ({ title, href }) => {
-  
     return (
       <m.div variants={centerLinkVars} className={`navLink text-5xl mt-8 uppercase  text-gray-200 `}>
         <Link href={href} onClick={() => {toggleMenu();}}>
@@ -86,17 +87,34 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
 
   const AddressTitle = () => {
     return(
-     <m.h3 className="text-3xl uppercase" variants={centerLinkVars}>Adres Biura:</m.h3>
+     <m.h3 className="text-3xl uppercase xl:pt-4" variants={centerLinkVars}>Adres Biura:</m.h3>
     )
   }
 
-  const InfoLinks = ({ address, addressHeader }) => {
-  
+  const WorkHoursTitle = () => {
+    return(
+     <m.h3 className="text-3xl uppercase xl:py-4" variants={centerLinkVars}>Dni i godziny pracy biura:</m.h3>
+    )
+  }
+
+  // const WorkHoursData = () => {
+  //   return (
+  //     <m.div variants={centerLinkVars} className="overflow-hidden">
+  //           {contactItems.map((item) =>(
+  //             <div className="flex justify-between overflow-hidden" key={item.id}>
+  //               <div className="py-2">{item.day}</div>
+  //               <div className="py-2">{item.hours}</div>
+  //             </div>
+  //           ))} 
+  //     </m.div>
+  //   );
+  // };
+  const AddressData = () => {
     return (
-      <m.div variants={centerLinkVars} className="mt-8 uppercase text-gray-200">
-          <div className="pb-10 w-full">
+      <m.div variants={centerLinkVars} className="mt-8  text-gray-200">
+          <div className="xl:pb-10 w-full">
           {contactItems.map((address, id) => (
-            <m.div className="mt-3 overflow-hidden" key={address.id} variants={centerLinkVars}>
+            <m.div className=" overflow-hidden" key={address.id} variants={centerLinkVars}>
               <Link
                 href="https://www.google.com/maps/place/Łódź, ul. Rewolucji 1905 r. nr 7"
                 target="_blank"
@@ -121,7 +139,8 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
           exit="initial"
           className={`page-nav flex fixed left-0 w-full h-screen origin-top 
                     border-t border-[#3e3e3e]
-                    bg-slate-950 text-gray-200 z-[50]`}
+                    bg-slate-950
+                    text-gray-200 z-[50]`}
         >
           {/* ----------------- LEFT ------------------ */}
           <m.div className="w-full h-full  flex-1 xl:border-r border-[#3e3e3e] xl:px-11"
@@ -134,15 +153,25 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
               <AddressTitle />
             </div>
             <div className="overflow-hidden" >
-              <InfoLinks />
+              <AddressData />
             </div>
-            <h3 className="text-3xl xl:mb-6  uppercase">dni i godziny pracy biura:</h3>
-            {contactItems.map((item) =>(
-              <div className="flex justify-between" key={item.id}>
-                <div className="py-2">{item.day}</div>
-                <div className="py-2">{item.hours}</div>
-              </div>
-            ))}
+            <div className="overflow-hidden">
+              <WorkHoursTitle />
+            </div>
+            <div>
+              {contactItems.map((item, id)=>(
+                <m.div className="flex justify-between overflow-hidden" 
+                       key={item.id}
+                       variants={containerVars}
+                       initial="initial"
+                       animate="open"
+                       exit="initial"                         
+                       >
+                  <m.div variants={centerLinkVars} className="py-2">{item.day}</m.div>
+                  <m.div variants={centerLinkVars} className="py-2">{item.hours}</m.div>
+                </m.div>
+              ))}
+            </div>
           </m.div>
           {/* ----------------- CENTER ------------------ */}
           <div className="h-full flex-1 flex-col border-r border-[#3e3e3e] px-8">
@@ -151,7 +180,7 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
               initial="initial"
               animate="open"
               exit="initial"
-              className="flex flex-col h-full  items-center gap-10 md:mb-12 xl:mb-12"
+              className="flex flex-col h-full items-center gap-10 md:mb-12 xl:mb-12"
             >
               {navLinks.map((link, index) => (
                 <div className="overflow-hidden" key={index}>
