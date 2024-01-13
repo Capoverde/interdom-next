2eab1d1ae949f7f7717ec7576ebaed31916e565e
Capoverde <piotr_as@yahoo.com>
2024-01-13 19:08:45 +0100

    moved objects to mailAndPhones

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -7,9 +7,7 @@ import { centerLinkVars } from "./helpers/centerLinkVars"
 import { boxVars } from "./helpers/boxVars"
 import contactItems from './NavbarData/contactItems.json'
 import navLinks from './NavbarData/navLinks.json'
-import { IoMdMail } from 'react-icons/io';
-import { FaPhoneAlt, FaMobile, FaLinkedin } from 'react-icons/fa';
-import { FaSquareFacebook } from "react-icons/fa6";
+import { MailAndPhones } from "./NavbarData/mailAndPhones";
 import './Navbar.scss'
 
 export const Navbar = ({ toggleMenu, isOpen }) => {
@@ -25,15 +23,6 @@ export const Navbar = ({ toggleMenu, isOpen }) => {
       document.body.style.overflow = 'unset';
     };
   }, [isOpen]);
-
-   const MailAndPhones = [
-    { id: 1, href: "biuro@interdom-nieruchomosci.pl", mail: "biuro@interdom-nieruchomosci.pl", icon: <IoMdMail /> },
-    { id: 2, href: "+42 630 84 55", phone1: "+42 630 84 55", icon: <FaPhoneAlt /> },
-    { id: 3, href: "+48 530 223 625", phone2: "+48 530 223 625", icon: <FaMobile /> },
-    { id: 4, href: "https://www.facebook.com/", title: "sprawdź nasz Facebook", facebook: "Facebook", socIcon: <FaSquareFacebook /> },
-    { id: 5, href: "https://www.linkedin.com/", title: "sprawdź nasz profil na LinkedIn", linkedin: "LinkedIn", socIcon: <FaLinkedin /> }
-  ];
-  
 
   const NavLinks = ({ title, href }) => {
     return (
--- app/components/Navbar/NavbarData/mailAndPhones.js
+++ app/components/Navbar/NavbarData/mailAndPhones.js
@@ -1,10 +1,11 @@
 import { IoMdMail } from 'react-icons/io';
-import { FaPhoneAlt, FaMobile, FaSquareFacebook, FaLinkedin } from 'react-icons/fa';
+import { FaPhoneAlt, FaMobile, FaLinkedin } from 'react-icons/fa';
+import { FaSquareFacebook } from "react-icons/fa6";
 
 export const MailAndPhones = [
   { id: 1, href: "biuro@interdom-nieruchomosci.pl", mail: "biuro@interdom-nieruchomosci.pl", icon: <IoMdMail /> },
   { id: 2, href: "+42 630 84 55", phone1: "+42 630 84 55", icon: <FaPhoneAlt /> },
   { id: 3, href: "+48 530 223 625", phone2: "+48 530 223 625", icon: <FaMobile /> },
-  { id: 4, href: "www.facebook.com", facebook: "Facebook", icon: <FaSquareFacebook /> },
-  { id: 5, href: "www.linkedin.com", linkedin: "LinkedIn", icon: <FaLinkedin /> }
+  { id: 4, href: "https://www.facebook.com/", title: "sprawdź nasz Facebook", facebook: "Facebook", socIcon: <FaSquareFacebook /> },
+  { id: 5, href: "https://www.linkedin.com/", title: "sprawdź nasz profil na LinkedIn", linkedin: "LinkedIn", socIcon: <FaLinkedin /> }
 ];
