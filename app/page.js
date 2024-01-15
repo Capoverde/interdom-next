6d54afe3880e187c0b182c1a0f726117f4b93cc2
Capoverde <piotr_as@yahoo.com>
2024-01-15 01:31:24 +0100

    grid in AboutPage - beginning

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 "use client"
-import { Navbar } from "./components/Navbar/Navbar";
 import { Header } from "./components/Header/Header";
-import { useState } from "react";
 import { HomePage } from "./pages/Home/HomePage";
-import { ScoreBanner } from './components/ScoreBanner/ScoreBanner'
 import { About } from './pages/About/AboutPage'
 import { Offer } from './pages/Offer/Offer'
 import { ContactPage } from "./pages/ContactPage/ContactPage";
@@ -14,7 +11,6 @@ export default function Home() {
     <>
       <Header />
       <HomePage />
-      <ScoreBanner />
       <About />
       <Offer />
       <ContactPage />
